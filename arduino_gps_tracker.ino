a451a2d8e9cea8e2fac2e7bf99b0d5de07bb2ff8
Un1XX388 <un1xx388@gmail.com>
2023-03-14 22:20:16 -0600

    Turned off button because it uses the same pin as the ethernet shield's SD card reader, add a bunch of server code to the main file. It still needs to be tested, customized, I took it from the WebServer Arduino project, cleaned up, and moved to it's own file.

--- arduino_gps_tracker.ino
+++ arduino_gps_tracker.ino
@@ -17,7 +17,16 @@ Led myLed;
 MyBuzzer myBuzzer;
 Output* myOutput;
 Potentiometer* myPot;
+// TODO: Move.
 bool error = false;
+// TODO: Move and break out port into constent.
+EthernetServer server(80);
+// TODO: Move.
+IPAddress IP(192, 168, 12, 2);
+// TODO: Move.
+byte MAC[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+};
 
 
 void setup() {
@@ -35,58 +44,10 @@ void setup() {
   }
   
   /* Ethernet Setup */
-  EthernetServer server(80);
-  byte MAC[] = {
-    0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
-  };
-  IPAddress IP(192, 168, 12, 2);
   // Start the Ethernet connection.
   Ethernet.begin(MAC, IP);
-  // Ethernet error checking.
-  while (Ethernet.hardwareStatus() == EthernetNoHardware) {
-    if (!error) {
-      // Display error message.
-      myOutput->addOutput("Ethernet shield");
-      myOutput->addOutput("not found.");
-      myOutput->publish();
-      // Turn buzzer one.
-      myBuzzer.on();
-      // Tack error state.
-      error = true;
-    }
-    // Flip state of LED.
-    //if (myLed.state() == HIGH) {
-    //  myLed.off();
-    //} else {
-    //  myLed.on();
-    //}
-  }
-  // Error resolved.
-  error = false;
-
-  while (Ethernet.linkStatus() == LinkOFF) {
-    if (!error) {
-      // Display error message.
-      myOutput->addOutput("Ethernet cable not connected.");
-      myOutput->publish();
-      // Turn buzzer on.
-      myBuzzer.on();
-      // Track error state.
-      error = true;
-    }
-    // Flit state of LED.
-    if (digitalRead(LED_PIN)) {
-      myLed.off();
-    } else {
-      myLed.on();
-    }
-  }
-  // Error resolved.
-  error = false;
-
-  // start the server
-  //server.begin();
-  //myOutput->addPersistentOutputText("IP:    ", String(Ethernet.localIP()));
+  // Start the server.
+  server.begin();
 
   /* Input Setup */
   myPot = new Potentiometer();
@@ -101,6 +62,62 @@ void setup() {
 }
 
 void loop() {
+  // Error check ethernet hardware.
+  errorCheckEthernetShieldConnected();
+  errorCheckEthernetCableConnected();
+  // Display network info.
+  myOutput->addOutput("IP:     ", String(Ethernet.localIP()));
+  // Listen for incoming clients.
+  EthernetClient client = server.available();
+  if (client) {
+    //Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        //Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+          // output the value of each analog input pin
+          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
+            int sensorReading = analogRead(analogChannel);
+            client.print("analog input ");
+            client.print(analogChannel);
+            client.print(" is ");
+            client.print(sensorReading);
+            client.println("<br />");
+          }
+          client.println("</html>");
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+    //Serial.println("client disconnected");
+  }
+
+  
   /* Potentiometer */
   myOutput->addOutput("Pot:        ", String(myPot->val()));
   int potVal = myPot->val(0, 100);
@@ -146,4 +163,54 @@ void loop() {
   myOutput->publish();
   
   delay(LOOP_WAIT);
+}
+
+// TODO: Move out of this file.
+void errorCheckEthernetShieldConnected() {
+  while (Ethernet.hardwareStatus() == EthernetNoHardware) {
+    if (!error) {
+      // Display error message.
+      myOutput->addOutput("Ethernet shield");
+      myOutput->addOutput("not found.");
+      myOutput->publish();
+      // Turn buzzer one.
+      myBuzzer.on();
+      // Tack error state.
+      error = true;
+    }
+    // Flip state of LED.
+    if (myLed.state()) {
+      myLed.off();
+    } else {
+      myLed.on();
+    }
+    delay(250);
+  }
+  // Error resolved.
+  error = false;
+}
+
+// TODO: Move out of this file.
+void errorCheckEthernetCableConnected() {
+  while (Ethernet.linkStatus() == LinkOFF) {
+    if (!error) {
+      // Display error message.
+      myOutput->addOutput("Ethernet cable");
+      myOutput->addOutput("not connected.");
+      myOutput->publish();
+      // Turn buzzer on.
+      myBuzzer.on();
+      // Track error state.
+      error = true;
+    }
+    // Flit state of LED.
+    if (myLed.state()) {
+      myLed.off();
+    } else {
+      myLed.on();
+    }
+    delay(250);
+  }
+  // Error resolved.
+  error = false;
 }
--- hardware_config.h
+++ hardware_config.h
@@ -1,17 +1,18 @@
 /** Define Macro(s) **/
 /* Assign Pin(s) */
 #define POTENTIOMETER_PIN A0
+// TODO:  Button pin works when set to 4, but that conflicts with ethernet shield, need to sort out.
 #define BUTTON_PIN 7
 #define BUZZER_PIN 5
 #define LED_PIN 6
 #define DHTPIN 3
 /* Set What Hardware is Used */
 #define USE_OLED true
-#define USE_SERIAL true
+#define USE_SERIAL false
 #define USE_PRESSURE_SENSOR true
-#define USE_BUTTON true
-#define USE_LED true
-#define USE_BUZZER true
+#define USE_BUTTON false
+#define USE_LED false
+#define USE_BUZZER false
 /* Default Value(s) */
 #define DEFAULT_BUZZER_TONE 30
 #define LOOP_WAIT 0
